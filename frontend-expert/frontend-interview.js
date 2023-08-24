50773789556208b9a50810fcdfe9caacad382c6e
BitFlippa27 <32537389+BitFlippa27@users.noreply.github.com>
2023-08-24 16:32:29 +0200

    submissionById

--- frontend-expert/frontend-interview.js
+++ frontend-expert/frontend-interview.js
@@ -1,4 +1,5 @@
-const QUESTION_API = "http://www.algoexpert.io/questions";
+const QUESTION_API = "https://www.algoexpert.io/questions";
+const SUBMISSION_API ="https://www.algoexpert.io/api/fe/submissions";
 
 const questionsRaw = 
 [
@@ -17,24 +18,60 @@ const questionsRaw =
     name: "Input",
     category: "JS",
   },
+  {
+    id: "event",
+    name: "Event Driven Programming",
+    category: "DOM",
+  },
 ];
 
-async function getQuestions() {
-  try {
-    const res = await fetch(QUESTION_API);
-    const questions = await res.json();
+const submissionsRaw = 
+[
+  {
+    questionId: "sign-up-form",
+    status: "CORRECT"
+  },
+  {
+    questionId: "rainbow-circles",
+    status: "INCORRECT"
+  },
+  {
+    questionId: "input",
+    status: "PARTIALLY_CORRECT"
+  },
+  {
+    questionId: "event",
+  },
 
-    return questions;
+];
+
+async function getQuestionsAndSubmissions() {
+  try {
+    //const subRes = await fetch(SUBMISSION_API);
+    //const questRes = await fetch(QUESTION_API);
+    const questions = await questRes.json();
+    const submissions = await subRes.json();
+
+    const [questRes, subRes] = Promise.all([
+      fetch(SUBMISSION_API),
+      fetch(QUESTION_API)
+    ]);
+
+    return Promise.all([
+      questRes.json(),
+      subRes.json()
+    ]);
+
   } catch (error) {
     console.log(error);
   }
 }
 
 
-function groupQuestionsByCategory(questions) {
+function getQuestionsByCategory(questionsRaw) {
   const questionsByCategory = {};
 
-  for (const question of questions) {
+  for (const question of questionsRaw) {
     if (questionsByCategory.hasOwnProperty(question.category)) {
       questionsByCategory[question.category].push(question);
     }
@@ -46,10 +83,19 @@ function groupQuestionsByCategory(questions) {
   return questionsByCategory;
 }
 
+function getSubmissionsByID(submissionsRaw) {
+  const submissionsByID = {};
+
+  for (submission of submissionsRaw) {
+    submissionsByID[submission.questionId] = submission.status;
+  }
+  console.log(submissionsByID);
+  return submissionsByID;
+}
+
 
 //Map Alternative
-
-function groupQuestionsByCategoryMap(questions) {
+/*function groupQuestionsByCategoryMap(questions) {
   const questionsByCategory = new Map();
 
   for (const question of questions) {
@@ -63,30 +109,42 @@ function groupQuestionsByCategoryMap(questions) {
   
   return questionsByCategory;
 }
+*/
 /*
 <div class="row">
 <div class="category column">
   <h2>HTML</h2>
   <div class="question">
+  <div class="status partially-correct"> * </div>
     <h3>Stopwatch</h3>
   </div>
   <div class="question">
+  <div class="status correct"> </div>
     <h3>Tic Tac Toe</h3>
   </div>
 </div>
 */
 
-function createCategoryDiv(category, questions) {
+function createCategoryDiv(category, questions, submissionsByID) {
   const categoryDiv = document.createElement("div");
   categoryDiv.classList.add("category");
   categoryDiv.classList.add("column");
   const categoryNameH2 = document.createElement("h2");
   categoryNameH2.textContent = category;
   categoryDiv.append(categoryNameH2);
+  
 
   for (question of questions) {
     const questionDiv = document.createElement("div");
     questionDiv.classList.add("question");
+    const submissionDiv = document.createElement("div");
+    submissionDiv.textContent = "*";
+    submissionDiv.classList.add("status");
+    
+    const statusClass = submissionsByID[question.id]?.toLowerCase().replace("_", "-");
+    submissionDiv.classList.add(statusClass ?? "unattempted"); //undefinded or null -> if there is no status
+
+    questionDiv.append(submissionDiv);
     const questionDivH3 = document.createElement("h3");
     questionDivH3.textContent = question.name;
     questionDiv.append(questionDivH3);
@@ -99,12 +157,12 @@ function createCategoryDiv(category, questions) {
 
 
 function fetchAndDisplay() {
-  //fetch();
-  const questionsByCategory = groupQuestionsByCategory(questionsRaw);
-  console.log(questionsByCategory);
+  // const [questions, submissions] = await fetchQuestionsAndSubmissions();
+  const questionsByCategory = getQuestionsByCategory(questionsRaw); //getQuestionsByCategory(questions)
+  const submissionsByID = getSubmissionsByID(submissionsRaw); // getQuestionByID(submissions)
 
   for (const [category, questions] of Object.entries(questionsByCategory)) {
-    const categoryDiv = createCategoryDiv(category, questions);
+    const categoryDiv = createCategoryDiv(category, questions, submissionsByID);
     console.log(categoryDiv);
 
     //const categoryWrapper = document.createElement("div");
